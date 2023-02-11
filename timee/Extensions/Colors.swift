c0fd0bbe6a21d016af8845cba55d1dde34ca386c
Sergii Simakhin <sergio@mrsergio.com>
2023-02-11 16:31:59 +0700

    Add color palette

---
+++ timee/Extensions/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  timee
+//
+//  Created by Sergii Simakhin on 2/11/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let Warning = Color("Warning")
+    static let LightYellow = Color("LightYellow")
+    static let DarkYellow = Color("DarkYellow")
+    static let LightPurple = Color("LightPurple")
+    static let DarkPurple = Color("DarkPurple")
+}
+
