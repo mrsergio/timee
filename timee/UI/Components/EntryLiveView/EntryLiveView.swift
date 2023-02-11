bdc9cf53b5e8cba83f9c800ace2027eefed7393f
Sergii Simakhin <sergio@mrsergio.com>
2023-02-11 14:10:53 +0700

    Add `EntryLiveView` with a basic idle and active states

---
+++ timee/UI/Components/EntryLiveView/EntryLiveView.swift
@@ -0,0 +1,76 @@
+//
+//  EntryLiveView.swift
+//  timee
+//
+//  Created by Sergii Simakhin on 2/11/23.
+//
+
+import SwiftUI
+
+enum EntryState {
+    case idle, active
+}
+
+struct EntryLiveView: View {
+    private let accentColor = Color.gray
+    
+    @State var entry: String = ""
+    @State var state: EntryState
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            ZStack {
+                Capsule()
+                    .strokeBorder(accentColor.opacity(0.25), lineWidth: 2)
+                
+                TextField("I'm working on...", text: $entry)
+                    .foregroundColor(accentColor)
+                    .padding([.leading, .trailing], 16)
+            }
+            .padding([.top, .bottom], 3)
+            
+            Button {
+                // action handler
+            } label: {
+                Image(systemName: buttonIconSystemName)
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .foregroundColor(buttonColor)
+                    .background(buttonColor.opacity(0.15))
+                    .clipShape(Circle())
+            }
+        }
+    }
+    
+    private var buttonIconSystemName: String {
+        switch state {
+            case .idle:
+                return "play.circle"
+            case .active:
+                return "stop.circle"
+        }
+    }
+    
+    private var buttonColor: Color {
+        switch state {
+            case .idle:
+                return Color.yellow
+            case .active:
+                return Color.red
+        }
+    }
+}
+
+struct EntryLiveView_Previews: PreviewProvider {
+    static var previews: some View {
+        EntryLiveView(state: .idle)
+            .frame(width: 300, height: 44)
+            .background(Color.gray.opacity(0.1))
+            .previewDisplayName("Idle")
+        
+        EntryLiveView(entry: "Patient #5", state: .active)
+            .frame(width: 300, height: 44)
+            .background(Color.gray.opacity(0.1))
+            .previewDisplayName("Active")
+    }
+}
