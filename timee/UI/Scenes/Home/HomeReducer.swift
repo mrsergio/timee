21bf5e51a992696767675104cbb62c9d6994d4e2
Sergii Simakhin <sergio@mrsergio.com>
2023-02-11 19:07:11 +0700

    Add HomeReducer (act as a `HomeView` model)

--- timee/UI/Scenes/Home/HomeReducer.swift
+++ timee/UI/Scenes/Home/HomeReducer.swift
@@ -5,4 +5,28 @@
 //  Created by Sergii Simakhin on 2/11/23.
 //
 
-import Foundation
+import ComposableArchitecture
+
+struct HomeReducer: ReducerProtocol {
+    
+    struct State: Equatable {
+        var entries: [Entry] = []
+    }
+    
+    enum Action: Equatable {
+        case add(Entry)
+        case delete(Entry)
+    }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        print("action: \(action)")
+        
+        switch action {
+            case .add(let entry):
+                return .none
+                
+            case .delete(let entry):
+                return .none
+        }
+    }
+}
