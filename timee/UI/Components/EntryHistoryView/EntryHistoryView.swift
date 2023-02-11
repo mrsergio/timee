47e19012f99adf009c7ac632b87063b6ae808c21
Sergii Simakhin <sergio@mrsergio.com>
2023-02-11 14:58:10 +0700

    Add `EntryHistoryView`

---
+++ timee/UI/Components/EntryHistoryView/EntryHistoryView.swift
@@ -0,0 +1,46 @@
+//
+//  EntryHistoryView.swift
+//  timee
+//
+//  Created by Sergii Simakhin on 2/11/23.
+//
+
+import SwiftUI
+
+struct EntryHistoryView: View {
+    @State var entry: String
+    @State var duration: String
+    @State var when: String
+    
+    var body: some View {
+        HStack() {
+            Text(entry)
+                .font(.headline)
+            
+            Spacer()
+            
+            VStack(alignment: .trailing) {
+                Text(when)
+                    .font(.footnote)
+                
+                Text(duration)
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+            }
+        }
+        .padding([.leading, .trailing], 16)
+    }
+}
+
+struct EntryHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        EntryHistoryView(
+            entry: "Patient #5",
+            duration: "0:00:34",
+            when: "today"
+        )
+        .frame(width: 300, height: 44)
+        .background(Color.gray.opacity(0.1))
+        .previewDisplayName("Idle")
+    }
+}
