b825da822b38e738cb24169c20af24935d2072b3
Sergii Simakhin <sergio@mrsergio.com>
2023-02-12 20:01:47 +0700

    Refactor `EntryLiveView` with an updated button handling logic

--- timee/UI/Components/EntryLiveView/EntryLiveView.swift
+++ timee/UI/Components/EntryLiveView/EntryLiveView.swift
@@ -7,35 +7,54 @@
 
 import SwiftUI
 
-enum EntryState {
-    case idle, active
-}
-
 struct EntryLiveView: View {
     private let accentColor = Color.LightYellow
+    private let placeholder: String = "I'm working on..."
     
-    @State var entry: String
-    @State var state: EntryState
+    @State private var title: String = ""
+    
+    /// View changes according to the entry state
+    enum EntryState {
+        case idle, active
+    }
+    let state: EntryState
+    
+    /**
+     String = current title (it could be edited before and while timer is on)
+    */
+    var onStart: (String) -> Void
+    var onStop: (String) -> Void
     
     var body: some View {
         HStack(spacing: 8) {
+            /* Text input wrapped with a capsule stroke */
             ZStack {
                 Capsule()
-                    .strokeBorder(accentColor.opacity(0.25), lineWidth: 2)
+                    .strokeBorder(
+                        accentColor.opacity(0.25),
+                        lineWidth: 2
+                    )
                 
                 TextField(
                     "",
-                    text: $entry,
-                    prompt: Text("I'm working on...")
+                    text: $title,
+                    prompt: Text(placeholder)
                         .foregroundColor(accentColor.opacity(0.75))
                 )
+                .onSubmit {
+                    // Start an entry timer when entry was in idle state; otherwise update the title only
+                    if state == .idle {
+                        onSubmit()
+                    }
+                }
                 .foregroundColor(accentColor)
                 .padding([.leading, .trailing], 16)
             }
             .padding([.top, .bottom], 3)
             
+            /* Play / pause button */
             Button {
-                // action handler
+                onSubmit()
             } label: {
                 Image(systemName: buttonIconSystemName)
                     .resizable()
@@ -47,35 +66,56 @@ struct EntryLiveView: View {
         }
     }
     
+    private func onSubmit() {
+        switch state {
+            case .idle:
+                // Starting time tracking session
+                onStart(title)
+                
+            case .active:
+                // Finishing time tracking session
+                onStop(title)
+        }
+    }
+    
     private var buttonIconSystemName: String {
         switch state {
             case .idle:
                 return "play.circle"
             case .active:
                 return "stop.circle"
+                
         }
     }
     
     private var buttonColor: Color {
         switch state {
             case .idle:
-                return Color.yellow
+                return Color.LightYellow
             case .active:
-                return Color.red
+                return Color.Orange
         }
     }
 }
 
 struct EntryLiveView_Previews: PreviewProvider {
     static var previews: some View {
-        EntryLiveView(entry: "", state: .idle)
-            .frame(width: 300, height: 44)
-            .background(Color.DarkPurple)
-            .previewDisplayName("Idle")
+        EntryLiveView(
+            state: .idle,
+            onStart: { _ in },
+            onStop: { _ in }
+        )
+        .frame(width: 300, height: 44)
+        .background(Color.DarkPurple)
+        .previewDisplayName("Idle")
         
-        EntryLiveView(entry: "Patient #5", state: .active)
-            .frame(width: 300, height: 44)
-            .background(Color.DarkPurple)
-            .previewDisplayName("Active")
+        EntryLiveView(
+            state: .active,
+            onStart: { _ in },
+            onStop: { _ in }
+        )
+        .frame(width: 300, height: 44)
+        .background(Color.DarkPurple)
+        .previewDisplayName("Active")
     }
 }
