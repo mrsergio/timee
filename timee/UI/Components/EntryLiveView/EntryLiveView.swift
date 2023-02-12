d7d3729b2f2b29e267aea6b3ad5bd0ed9ded207c
Sergii Simakhin <sergio@mrsergio.com>
2023-02-12 22:06:31 +0700

    Add a timer to the `EntryLiveView`

--- timee/UI/Components/EntryLiveView/EntryLiveView.swift
+++ timee/UI/Components/EntryLiveView/EntryLiveView.swift
@@ -11,11 +11,12 @@ struct EntryLiveView: View {
     private let accentColor = Color.LightYellow
     private let placeholder: String = "I'm working on..."
     
+    @FocusState private var isFocused: Bool
     @State private var title: String = ""
     
     /// View changes according to the entry state
     enum EntryState {
-        case idle, active
+        case idle, active(Double)
     }
     let state: EntryState
     
@@ -26,47 +27,71 @@ struct EntryLiveView: View {
     var onStop: (String) -> Void
     
     var body: some View {
-        HStack(spacing: 8) {
-            /* Text input wrapped with a capsule stroke */
-            ZStack {
-                Capsule()
-                    .strokeBorder(
-                        accentColor.opacity(0.25),
-                        lineWidth: 2
+        VStack(spacing: 8) {
+            HStack(spacing: 8) {
+                /* Text input wrapped with a capsule stroke */
+                ZStack {
+                    Capsule()
+                        .strokeBorder(
+                            accentColor.opacity(0.25),
+                            lineWidth: 2
+                        )
+                    
+                    TextField(
+                        "",
+                        text: $title,
+                        prompt: Text(placeholder)
+                            .foregroundColor(textForegroundColor.opacity(0.75))
                     )
-                
-                TextField(
-                    "",
-                    text: $title,
-                    prompt: Text(placeholder)
-                        .foregroundColor(accentColor.opacity(0.75))
-                )
-                .onSubmit {
-                    // Start an entry timer when entry was in idle state; otherwise update the title only
-                    if state == .idle {
-                        onSubmit()
+                    .focused($isFocused)
+                    .onSubmit {
+                        // Start an entry timer when entry was in idle state; otherwise update the title only
+                        switch state {
+                            case .idle:
+                                onSubmit()
+                                
+                            default:
+                                break
+                        }
                     }
+                    .italic()
+                    .foregroundColor(textForegroundColor)
+                    .padding([.leading, .trailing], 16)
                 }
-                .foregroundColor(accentColor)
-                .padding([.leading, .trailing], 16)
+                .padding([.top, .bottom], 3)
+                
+                /* Play / pause button */
+                Button {
+                    onSubmit()
+                } label: {
+                    Image(systemName: buttonIconSystemName)
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .foregroundColor(buttonColor)
+                        .background(buttonColor.opacity(0.15))
+                        .clipShape(Circle())
+                }
             }
-            .padding([.top, .bottom], 3)
+            .frame(height: 44)
             
-            /* Play / pause button */
-            Button {
-                onSubmit()
-            } label: {
-                Image(systemName: buttonIconSystemName)
-                    .resizable()
-                    .aspectRatio(1, contentMode: .fit)
-                    .foregroundColor(buttonColor)
-                    .background(buttonColor.opacity(0.15))
-                    .clipShape(Circle())
+            /* Display timer while in active state */
+            switch state {
+                case .active(let duration):
+                    Text(duration.humanReadableTime)
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.Orange)
+                    
+                default:
+                    EmptyView()
             }
         }
     }
     
     private func onSubmit() {
+        // Dismiss the keyboard
+        isFocused = false
+        
         switch state {
             case .idle:
                 // Starting time tracking session
@@ -75,6 +100,18 @@ struct EntryLiveView: View {
             case .active:
                 // Finishing time tracking session
                 onStop(title)
+                
+                // Cleanup the input field for a next entry
+                title = ""
+        }
+    }
+    
+    private var textForegroundColor: Color {
+        switch state {
+            case .idle:
+                return accentColor
+            case .active:
+                return Color.Orange
         }
     }
     
@@ -105,17 +142,19 @@ struct EntryLiveView_Previews: PreviewProvider {
             onStart: { _ in },
             onStop: { _ in }
         )
-        .frame(width: 300, height: 44)
         .background(Color.DarkPurple)
+        .padding([.top, .bottom], 16)
+        .padding([.leading, .trailing], 12)
         .previewDisplayName("Idle")
         
         EntryLiveView(
-            state: .active,
+            state: .active(35),
             onStart: { _ in },
             onStop: { _ in }
         )
-        .frame(width: 300, height: 44)
         .background(Color.DarkPurple)
+        .padding([.top, .bottom], 16)
+        .padding([.leading, .trailing], 12)
         .previewDisplayName("Active")
     }
 }
