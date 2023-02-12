0512038a35e3605384244cb47ab3b233833c0608
Sergii Simakhin <sergio@mrsergio.com>
2023-02-12 19:54:57 +0700

    Refactor `Entry` with an `endDate` and computed `duration`

--- timee/Persistence/Entry.swift
+++ timee/Persistence/Entry.swift
@@ -9,16 +9,45 @@ import Foundation
 import GRDB
 
 struct Entry: Identifiable, Hashable {
+    internal init(id: Int64? = nil, title: String = "", startDate: Date = Date(), endDate: Date? = nil) {
+        self.id = id
+        self.title = title
+        self.startDate = startDate
+        self.endDate = endDate
+    }
+    
     var id: Int64?
     var title: String
-    var duration: Double // in seconds
-    var date: Date
+    var startDate: Date
+    var endDate: Date?
+    
+    /// in seconds (computed variable, not stored in DB)
+    var duration: Double {
+        if let endDate {
+            let startDateComponents = Calendar.current
+                .dateComponents([.second], from: startDate)
+            
+            let endDateComponents = Calendar.current
+                .dateComponents([.second], from: endDate)
+            
+            let duration = Calendar.current
+                .dateComponents([.second], from: startDateComponents, to: endDateComponents)
+                .second
+            
+            return Double(abs(duration ?? 0))
+            
+        } else {
+            return 0.0
+        }
+    }
 }
+
+// MARK: - Database Protocols
 
 extension Entry: Codable, FetchableRecord, MutablePersistableRecord {
     // Define database columns from CodingKeys
     fileprivate enum Columns {
-        static let date = Column(CodingKeys.date)
+        static let date = Column(CodingKeys.startDate).desc // sort descending when ordering by date
     }
     
     // Update auto-incremented id upon successful insertion
@@ -27,7 +56,7 @@ extension Entry: Codable, FetchableRecord, MutablePersistableRecord {
     }
 }
 
-// MARK: - Entry Database Requests
+// MARK: - Database Requests
 
 extension DerivableRequest<Entry> {
     func orderedByDate() -> Self {
